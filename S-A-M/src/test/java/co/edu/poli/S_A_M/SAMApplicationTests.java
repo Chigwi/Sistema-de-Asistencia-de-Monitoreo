5a3796c7816c0fb669e160bcf8738b82539c20fa
allie <allie@Mariana>
2026-09-11 18:58:32 -0500

    añado plantilla de spring

---
+++ S-A-M/src/test/java/co/edu/poli/S_A_M/SAMApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.poli.S_A_M;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SAMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
