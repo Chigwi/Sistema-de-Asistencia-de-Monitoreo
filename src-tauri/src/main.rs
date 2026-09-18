540abd458cc5e4017f6c2305774c46d51448a1cc
Chigwi <allie.velandia@outlook.com>
2026-09-18 17:09:21 -0500

    añado instalación de diesel para ORM

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod db;
+
 fn main() {
     s_a_m_lib::run()
 }
